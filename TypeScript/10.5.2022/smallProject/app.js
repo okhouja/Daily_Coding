116e12d5acc958b6bff3928adaf80e8ffbd7ca8b
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-11 15:41:09 +0200

    TS Object types

--- TypeScript/10.5.2022/smallProject/app.js
+++ TypeScript/10.5.2022/smallProject/app.js
@@ -1,18 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    // if(typeof n1 !== 'number'|| typeof n2 !== 'number'){
-    //     throw new Error("Incorrect Input!");
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1;
-number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPharse = "Result is: ";
-add(number1, number2, printResult, resultPharse);
+// const person: {
+//     name:object;
+//     age:number;
+// } = {
+var person = {
+    name: "Omar",
+    age: 40
+};
+console.log(person.name);
