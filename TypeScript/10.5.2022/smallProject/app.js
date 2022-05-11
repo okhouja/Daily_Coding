0c5584573be634ea154c9366cd2c50425b9f48bc
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-11 17:08:01 +0200

    TS Litaral Types

--- TypeScript/10.5.2022/smallProject/app.js
+++ TypeScript/10.5.2022/smallProject/app.js
@@ -1,14 +1,21 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === 'as-number'){
+    //     return +result;
+    //     } else {
+    //         return result.toString();
+    //     }
 }
-var combineAges = combine(30, 26);
+var combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
-var combineNames = combine('Anna', 'Max');
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combineNames = combine('Anna', 'Max', 'as-text');
 console.log(combineNames);
