741c513bc0b38352d06d7328fae2bfbf49d7e302
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-14 11:13:55 +0200

    TS class Public & Private

--- TypeScript/14.05.2022/src/app.ts
+++ TypeScript/14.05.2022/src/app.ts
@@ -1,18 +1,34 @@
 class Department {
-  name: string;
-
+  public name: string;
+  private employees: string[] = [];
   constructor(n: string) {
     this.name = n;
   }
   describe(this: Department) {
     console.log("Department:" + this.name);
   }
+  addEmployee(employee: string) {
+    // Validation etc
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Acountting");
 
+accounting.addEmployee("Anna");
+accounting.addEmployee("Bob");
+
+// accounting.employees[2] = "Max";
+
 accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
 
-const accountingCopy={name:'Dummy',describe:accounting.describe}
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
 
-accountingCopy.describe()
+// accountingCopy.describe();
