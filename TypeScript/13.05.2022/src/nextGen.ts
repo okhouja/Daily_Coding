8838b6b750b6ab41123fb64b4742eba91dbd69ad
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-14 09:01:27 +0200

    TS nextGen JS ES6

--- TypeScript/13.05.2022/src/nextGen.ts
+++ TypeScript/13.05.2022/src/nextGen.ts
@@ -16,4 +16,14 @@
 
 // console.log(result);
 
-const add = (a: number, b: number) => a + b;
+// const add = (a: number, b: number) => a + b;
+
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
+
+// const button = document.querySelector("button");
+
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
+// printOutput(add(5));
