11883474638dd544dd7f240bb296ae61573d7143
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-16 19:24:07 +0200

    TS Classis Overriding properties & Protected

--- TypeScript/14.05.2022/src/app.ts
+++ TypeScript/14.05.2022/src/app.ts
@@ -1,7 +1,9 @@
 class Department {
   //   private readonly id: string;
   //   public name: string;
-  private employees: string[] = [];
+  //   private employees: string[] = [];
+  protected employees: string[] = [];
+
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
@@ -21,15 +23,54 @@ class Department {
   }
 }
 
-const accounting = new Department("d1", "Acountting");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee("Anna");
+class AcounttingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addEmployee(name: string) {
+    if (name === "Omar") {
+      return;
+    }
+    this.employees.push(name);
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Omar"]);
+
+it.addEmployee("Anna");
+it.addEmployee("Bob");
+
+// it.employees[2] = "Max";
+
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AcounttingDepartment("d2", []);
+
+accounting.addReport("Something went wrong!...");
+
+accounting.addEmployee("Maxi");
 accounting.addEmployee("Bob");
 
-// accounting.employees[2] = "Max";
-
-accounting.describe();
-accounting.name = "NEW NAME";
+accounting.printReports();
 accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: "Dummy", describe: accounting.describe };
