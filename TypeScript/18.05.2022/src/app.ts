8de12b65c48f8be1de086552946eecc4233193d0
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-18 12:01:30 +0200

    TS Intersection Types

--- TypeScript/18.05.2022/src/app.ts
+++ TypeScript/18.05.2022/src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privilages: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployess extends Admin, Employee{}
+
+type ElevatedEmployess = Admin & Employee;
+
+const e1: ElevatedEmployess = {
+  name: "Omar",
+  privilages: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
