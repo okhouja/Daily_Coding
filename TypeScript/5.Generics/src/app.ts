197332eb99e35f25e005e636ccf3bf8d59395406
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-20 15:57:16 +0200

    TS Built-in Generics

--- TypeScript/5.Generics/src/app.ts
+++ TypeScript/5.Generics/src/app.ts
@@ -1,1 +1,13 @@
 /* *** Built-in Generics *** */
+
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+// promise.then((data: any) => {
+//   data.split(" ");
+// });
