3c387d1a287fdeef20f689ee68ea49f988e0ee27
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-20 16:29:40 +0200

    TS Create a Generic function

--- TypeScript/5.Generics/src/app.ts
+++ TypeScript/5.Generics/src/app.ts
@@ -11,3 +11,12 @@
 // promise.then((data: any) => {
 //   data.split(" ");
 // });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+const mergeObj = merge(
+  { name: "Omar", hobbies: ["Sports", "Cooking"] },
+  { age: 40 }
+);
+console.log(mergeObj);
