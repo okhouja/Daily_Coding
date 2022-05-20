8b1ac262bd56baa3b7da44016f5e10e4f354a79b
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-20 16:51:35 +0200

    TS another Generic functions

--- TypeScript/5.Generics/src/app.ts
+++ TypeScript/5.Generics/src/app.ts
@@ -20,3 +20,21 @@ const mergeObj = merge(
   { age: 40 }
 );
 console.log(mergeObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got" + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+console.log(countAndDescribe(["sports", "Cooking"]));
+console.log(countAndDescribe("Hi"));
+console.log(countAndDescribe(""));
+console.log(countAndDescribe("T"));
