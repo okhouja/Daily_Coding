cbefdf0b531609f8fc0a154618d63b13a656301a
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-20 13:11:22 +0200

    TS Nullish Coalascing

--- TypeScript/18.05.2022/src/app.ts
+++ TypeScript/18.05.2022/src/app.ts
@@ -1,3 +1,5 @@
+/* *** Intersection Type *** */
+
 type Admin = {
   name: string;
   privileges: string[];
@@ -22,12 +24,21 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+/* End of Intersection Type */
 
-// Function Overloads
+// Function Overloads and Type Guard
+
 function add(a: number, b: number): number;
 function add(a: string, b: string): string;
 function add(a: string, b: number): string;
 function add(a: number, b: string): string;
+
+/* Function Type Guard
+What's a "Type Guard"?
+- A code pattern where you check for ceertain type before you try to do something with it at runtime.
+ * so With type guards you avoid runtime errors by checking types before you try to do something with the values.
+*/
+
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
@@ -36,14 +47,6 @@ function add(a: Combinable, b: Combinable) {
 }
 const result = add("John", "Smith");
 result.split(" ");
-
-// Optionam Chaining
-const fetchedUserData = {
-  id: "u1",
-  name: "Paul",
-  job: { title: "CEO", description: "My own Company" },
-};
-console.log(fetchedUserData?.job?.title);
 
 type UnknownEmployee = Employee | Admin;
 
@@ -85,6 +88,9 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+/* End of Type Gaurd*/
+
+/*  *** Discriminated Type *** */
 
 interface Bird {
   type: "bird";
@@ -111,6 +117,9 @@ function moveAnimal(animal: Animal) {
 
 moveAnimal({ type: "horse", runningSpeed: 10 });
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+/* */
+
+/* *** Type Casting *** */
 
 // const paragraph = document.querySelector("p");
 // const paragraph = document.getElementById("message-output");
@@ -135,3 +144,24 @@ const errorBag: ErrorContainer = {
   email: "Not a valid email",
   username: "Must start with a character!",
 };
+/* */
+
+/* *** Optionam Chaining *** */
+
+const fetchedUserData = {
+  id: "u1",
+  name: "Paul",
+  job: { title: "CEO", description: "My own Company" },
+};
+console.log(fetchedUserData?.job?.title);
+/* */
+
+// Nullish Coalescing
+
+// const userInput = "";
+const userInput = undefined;
+
+const storedData = userInput ?? "DEFAULT";
+
+console.log(storedData);
+/* */
