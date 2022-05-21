42f94bb8ff2f10379bcd890423140185e9ddeaa8
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-21 17:31:29 +0200

    TS First Class Decorators

--- TypeScript/6.Decorators/src/app.ts
+++ TypeScript/6.Decorators/src/app.ts
@@ -1,1 +1,16 @@
 /* A First Class Decorators */
+
+function Logger(constructor: Function) {
+  console.log("Logging...");
+  console.log(constructor);
+}
+@Logger
+class Person {
+  name = "Omar";
+
+  constructor() {
+    console.log("Creating person Object....");
+  }
+}
+const pers = new Person();
+console.log(pers);
