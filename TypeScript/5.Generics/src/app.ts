30d23cb7554c6e72e1934bc0cbff1b0dc60dccd9
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-05-21 14:59:11 +0200

    TS Generic Utility Types

--- TypeScript/5.Generics/src/app.ts
+++ TypeScript/5.Generics/src/app.ts
@@ -79,3 +79,24 @@ const numberStorage = new DataStorage<number>();
 // /* */
 // objStorage.removeItem(maxObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUnit: Date;
+}
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUnit = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Anna"];
+// names.push("Mark");
+// names.pop("");
