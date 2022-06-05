35357d6e4c32c8de2c4c99b5f859a0c1f98eb1fc
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-06-05 16:54:49 +0200

    TS finely new webpack start work

--- TypeScript/8.3rdPartyLib/src/app.ts
+++ TypeScript/8.3rdPartyLib/src/app.ts
@@ -1,3 +1,7 @@
-import _ from 'lodash'
+ import _ from 'lodash'
 
 console.log(_.shuffle([1,2,3,4]));
+
+console.log('f');
+
+
---
+++ TypeScript/8.3rdPartyLib/webpack.config.ts
@@ -0,0 +1,34 @@
+import path from "path";
+import * as webpack from "webpack";
+import "webpack-dev-server";
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: "./src/app.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
+  },
+  devtool: "inline-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
+
+export default config;
