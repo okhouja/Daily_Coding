638ba0651e89c5b5c1b9ebf3219b838dfe85d8da
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-06-07 14:54:29 +0200

    TS try to hide api key in html

---
+++ TypeScript/9.2.Project/src/ApiLink.ts
@@ -0,0 +1,2 @@
+const api_Key = process.env.GOOGLE_API_KEY;
+const apiUrl: string = `https://maps.googleapis.com/maps/api/js?key=${api_Key}`;
--- TypeScript/9.2.Project/webpack.config.ts
+++ TypeScript/9.2.Project/webpack.config.ts
@@ -2,6 +2,8 @@ import path from "path";
 import { Configuration } from "webpack";
 import "webpack-dev-server";
 import Dotenv from "dotenv-webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+const api_Key = process.env.GOOGLE_API_KEY;
 
 const config: Configuration = {
   mode: "development",
@@ -29,11 +31,20 @@ const config: Configuration = {
       },
     ],
   },
+  stats: { children: true },
   resolve: {
     extensions: [".ts", ".js"],
     fallback: { os: false },
   },
-  plugins: [new Dotenv()],
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: false,
+      env: process.env.NODE_ENV,
+      apiUrl: `https://maps.googleapis.com/maps/api/js?key=${api_Key}`,
+    }),
+  ],
 };
 
 export default config;
