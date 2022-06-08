a1006049d736e0cfdbe42bec79031594b45ee0ec
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-06-08 10:41:42 +0200

    Getting Uder Input with refs

--- TypeScript/10.react_typescript/src/App.tsx
+++ TypeScript/10.react_typescript/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the Course" }];
   return <div className="App">
-    {/* A component that add todos */}
+    <NewTodo />
     <TodoList items={todos} />
   </div>;
 };
