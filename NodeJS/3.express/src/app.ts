0dfe573c335b50e90a63dc82745d15a90ca2a6dd
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-05 12:46:19 +0200

    Parsing Incoming Request

--- NodeJS/3.express/src/app.ts
+++ NodeJS/3.express/src/app.ts
@@ -2,25 +2,34 @@
 
 import express, { Request, Response, NextFunction } from "express";
 
+import bodyParser from "body-parser";
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use((req, res, next) => {
 //   console.log("In the Middleware!");
 //   next(); // Allow the request to continue to the middleware in Line.
 // });
 
-app.use("/", (req, res, next) => {
-  console.log("This always run!");
-  next();
-});
+// app.use("/", (req, res, next) => {
+//   console.log("This always run!");
+//   next();
+// });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In Add Product!");
-  res.send('<h1> The "Add Product" Page! </h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In anther middleware!");
   res.send("<h1> Hello from Express! </h1>");
 });
 // const server = http.createServer(app);
