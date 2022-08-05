f2853f1d07dcdb41c007b45f961f1cc15d08ca9c
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-05 12:15:58 +0200

    Handling Diffrent Routes

--- NodeJS/3.express/src/app.ts
+++ NodeJS/3.express/src/app.ts
@@ -1,19 +1,30 @@
-const http = require("http");
+// const http = require("http");
 
 import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the Middleware!");
-  next(); // Allow the request to continue to the middleware in Line.
+// app.use((req, res, next) => {
+//   console.log("In the Middleware!");
+//   next(); // Allow the request to continue to the middleware in Line.
+// });
+
+app.use("/", (req, res, next) => {
+  console.log("This always run!");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In Add Product!");
+  res.send('<h1> The "Add Product" Page! </h1>');
+});
+
+app.use("/", (req, res, next) => {
   console.log("In anther middleware!");
   res.send("<h1> Hello from Express! </h1>");
 });
+// const server = http.createServer(app);
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is Running on port 3000!");
+});
