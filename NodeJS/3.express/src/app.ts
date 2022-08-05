cafa0376fac8b2122e82a9634a46dbd036a41355
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-05 18:43:50 +0200

    using Express Router

--- NodeJS/3.express/src/app.ts
+++ NodeJS/3.express/src/app.ts
@@ -1,10 +1,11 @@
 // const http = require("http");
 
 import express, { Request, Response, NextFunction } from "express";
-
 import bodyParser from "body-parser";
 
 const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,20 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //   next();
 // });
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>'
-  );
-});
+// app.use("/", (req, res, next) => {
+//   res.send("<h1> Hello from Express! </h1>");
+// });
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1> Hello from Express! </h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 // const server = http.createServer(app);
 
 app.listen(3000, () => {
---
+++ NodeJS/3.express/src/routes/admin.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+    res.send(
+      '<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>'
+    );
+  });
+  
+  router.post("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+  });
+
+  module.exports = router
