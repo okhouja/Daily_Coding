e3c38bdde5c36dc3326dfe047ae7da0b3a305d1f
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-06 13:11:59 +0200

    Serving Files Statically & Styling the Page

--- NodeJS/3.express/src/app.ts
+++ NodeJS/3.express/src/app.ts
@@ -8,6 +8,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use((req, res, next) => {
 //   console.log("In the Middleware!");
@@ -27,7 +28,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 // const server = http.createServer(app);
