83a53ae210c525a21fd1a0a5aa499a3044bbb58a
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-07 17:46:38 +0200

    output dynamic content

--- NodeJS/3.express/src/routes/shop.ts
+++ NodeJS/3.express/src/routes/shop.ts
@@ -4,15 +4,11 @@ const path = require("path");
 const rootDir = require("../util/path");
 const adminData = require("./admin");
 
-// import { render } from 'pug';
-
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  //  console.log('shop',adminData.products);
-  //  res.sendFile(path.join(rootDir, 'views','shop.html'))
-
-  res.render('shop');
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
