24bbb8787d8d563107f5f767271958ec49ce569c
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-07 18:22:00 +0200

    Converting html files to PUG

--- NodeJS/3.express/src/routes/admin.ts
+++ NodeJS/3.express/src/routes/admin.ts
@@ -3,11 +3,11 @@ const path = require("path");
 const rootDir = require("../util/path");
 const router = express.Router();
 
-const products:any = [];
+const products: any = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
 // /admin/edit-product => POST
