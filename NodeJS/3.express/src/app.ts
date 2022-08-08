cedb6c7f2141b688e242cafbc4430e6945bf645d
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-08 15:22:17 +0200

    working with Handlebars

--- NodeJS/3.express/src/app.ts
+++ NodeJS/3.express/src/app.ts
@@ -2,9 +2,14 @@ const path = require("path");
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 
+const expressHbs: any = require("express-handlebars");
+
 const app = express();
 
-app.set("view engine", "pug");
+// app.set("view engine", "pug");
+
+app.engine("hbs", expressHbs.engine({ extname: '.hbs', defaultLayout: ""}));
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
 const adminData = require("./routes/admin");
