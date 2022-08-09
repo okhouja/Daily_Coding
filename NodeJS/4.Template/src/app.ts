3f1681d31be988112593ce6a0fef968c5d6d75f4
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-09 15:08:06 +0200

    Template engine Assignment handlebars Hbs

--- NodeJS/4.Template/src/app.ts
+++ NodeJS/4.Template/src/app.ts
@@ -1,12 +1,20 @@
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
-import path from "path";
+const path = require("path");
+
+const expressHbs: any = require("express-handlebars");
 
 const app = express();
 
 const users: any = [];
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs.engine({ extname: "hbs", defaultLayout: "main-layout" })
+);
+// app.set("view engine", "pug");
+
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,12 +24,16 @@ app.get("/", (req, res, next) => {
 });
 
 app.get("/users", (req, res, next) => {
-  res.render("users", { pageTitle: "Users", users: users });
+  res.render("users", {
+    pageTitle: "Users",
+    users: users,
+    hasUsers: users.length > 0,
+  });
 });
 
 app.post("/add-user", (req, res, next) => {
   users.push({ name: req.body.username });
-  res.redirect("/");
+  res.redirect("/users");
 });
 
 app.listen(3000, () => {
