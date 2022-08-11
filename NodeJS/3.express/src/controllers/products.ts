a5c5277c0c081b81d0a4fea3771e8e79db7e2542
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-11 11:10:43 +0200

    Creating The Shop Structure

--- NodeJS/3.express/src/controllers/products.ts
+++ NodeJS/3.express/src/controllers/products.ts
@@ -6,7 +6,7 @@ const Product = require("../models/product");
 // const products : any = [];
 
 exports.getAddProduct = (req: Request, res: Response, next: NextFunction) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -23,7 +23,7 @@ exports.postAddProduct = (req: Request, res: Response, next: NextFunction) => {
 
 exports.getProduct = (req: Request, res: Response, next: NextFunction) => {
   Product.fetchAll((products: any) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
