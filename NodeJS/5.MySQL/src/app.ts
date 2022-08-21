23b443e29e619b636f31799933fa79570a8ea082
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-21 10:38:08 +0200

    Create & Mangment a Dummy User

--- NodeJS/5.MySQL/src/app.ts
+++ NodeJS/5.MySQL/src/app.ts
@@ -20,18 +20,38 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  User.findByPk(1)
+    .then((user:String) => {
+      req.user = user;
+      next();
+    })
+    .catch((err: Error) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product)
+User.hasMany(Product);
 
 sequelize
-  .sync({force: true})
+  // .sync({force: true})
+  .sync()
   .then((result: any) => {
+    return User.findByPk(1);
     // console.log(result);
+  })
+  .then((user: any) => {
+    if (!user) {
+      return User.create({ name: "Omar", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user: any) => {
+    // console.log(user);
     app.listen(3000, () => {
       console.log("Server is Running on port 3000!");
     });
