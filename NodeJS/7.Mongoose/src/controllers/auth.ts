ae94c57dae1529e5789fb019ea63f92725f84d69
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-30 20:38:33 +0200

    Add Token to Email link for Reset Password

--- NodeJS/7.Mongoose/src/controllers/auth.ts
+++ NodeJS/7.Mongoose/src/controllers/auth.ts
@@ -1,8 +1,12 @@
 import { RequestHandler } from "express";
 
+const crypto = require("crypto");
+
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const mongoose = require("mongoose");
+
+// console.log(process.env.My_Gmail);
 
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SG_API_KEY);
@@ -90,7 +94,7 @@ export const postSignup: RequestHandler = (req, res, next) => {
           res.redirect("/login");
           return sgMail.send({
             to: email,
-            from: "liebelos1@gmail.com",
+            from: process.env.My_Gmail,
             subject: "Signup succeeded!",
             html: "<h1>You Successfully signed up!</h1>",
             text: "Welcome to Node.js Shop Dev!",
@@ -110,6 +114,58 @@ export const postLogout: RequestHandler = (req, res, next) => {
   req.session.destroy((err: any) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+export const getReset: RequestHandler = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+export const postReset: RequestHandler = (req, res, next) => {
+  crypto.randomBytes(64, (err: any, buffer: any) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    console.log(token);
+    
+    User.findOne({ email: req.body.email })
+      .then((user:any) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result:any) => {
+        res.redirect("/");
+        sgMail.send({
+          to: req.body.email,
+          from: process.env.My_Gmail,
+          subject: "Password reset",
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+            <p>- If someone else triggered this request or if you've remembered your password in the meantime, you can simply ignore this message and carry on using your old password. For security reasons, this link is only valid for 1 hour, after which time you'll need to request a new one.</p>
+          `,
+        });
+      })
+      .catch((err:Error) => {
+        console.log(err);
+      });
   });
 };
 
