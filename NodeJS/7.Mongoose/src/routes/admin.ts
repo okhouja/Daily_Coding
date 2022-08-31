fc07529d8c09660c6877cf57e50f4ddea0de9d14
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-08-31 22:10:15 +0200

    Add Validator to Admin routes

--- NodeJS/7.Mongoose/src/routes/admin.ts
+++ NodeJS/7.Mongoose/src/routes/admin.ts
@@ -1,4 +1,7 @@
 import express from "express";
+
+import { body } from "express-validator";
+
 // const rootDir = require("../util/path");
 // const path = require("path");
 
@@ -14,11 +17,31 @@ router.get("/add-product", isAuth, adminController.getAddProduct);
 router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/edit-product => POST
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").isLength({ min: 5, max: 400 }).trim(),
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").isLength({ min: 5, max: 400 }).trim(),
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
