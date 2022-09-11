1a400d38860ca7816091316078869eb6d543b107
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-09-11 21:13:04 +0200

    setup mongoDB & mongoose

--- NodeJS/9.REST_API_app/server/src/app.ts
+++ NodeJS/9.REST_API_app/server/src/app.ts
@@ -1,8 +1,16 @@
-import express,{ RequestHandler } from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+const DB_HOST = process.env.DB_HOST;
+
+const path = require("path");
+
+import express, { Request, Response, NextFunction } from "express";
 
 import bodyParser from "body-parser";
 
-const feedRoutes = require('./routes/feed');
+import mongoose, { ConnectOptions } from "mongoose";
+
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
@@ -10,14 +18,38 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 app.listen(8080, () => {
-    console.log("Server is Running on port 3000!");
-  });
+  console.log("Server is Running on port 3000!");
+});
+
+mongoose
+  .connect(
+    DB_HOST as string,
+    { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions
+  )
+  .then(() => console.log("Connected to MongoDB!"))
+
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("Server is Running on port 3000!");
+    });
+  })
+  .catch((err: Error) => console.log(err));
