5d3eee4006e9f2b7b71fdee913d56e606f73b5b4
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-09-14 11:10:56 +0200

    add Upload & Delete & Pagination

--- NodeJS/9.REST_API_app/server/src/controllers/feed.ts
+++ NodeJS/9.REST_API_app/server/src/controllers/feed.ts
@@ -2,13 +2,27 @@ import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
 import Post from "../models/post";
 import mongoose from "mongoose";
+import fs from "fs";
+import path from "path";
+import { log } from "console";
 
 export const getPosts: RequestHandler = (req, res, next) => {
+  const currentPage: any = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
-    .then((posts: any) => {
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
+    .then((posts) => {
       res.status(200).json({
         message: "Fetched posts successfully.",
         posts: posts,
+        totalItems: totalItems,
       });
     })
     .catch((err) => {
@@ -27,7 +41,7 @@ export const createPost: RequestHandler = (req, res, next) => {
     throw error;
   }
   if (!req.file) {
-    const error = new Error('No image provided.');
+    const error = new Error("No image provided.");
     error.statusCode = 422;
     throw error;
   }
@@ -68,6 +82,85 @@ export const getPost: RequestHandler = (req, res, next) => {
         throw error;
       }
       res.status(200).json({ message: "Post fetched.", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath: any) => {
+  filePath = path.join(filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
+
+export const updatePost: RequestHandler = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file?.path.replace(/\\/g, "/");
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked.");
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post updated!", post: result });
+      
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+export const deletePost: RequestHandler = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      // Check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "Deleted post." });
+      console.log(result,"Deleted post.");
+      
+
     })
     .catch((err) => {
       if (!err.statusCode) {
--- NodeJS/9.REST_API_app/server/src/routes/feed.ts
+++ NodeJS/9.REST_API_app/server/src/routes/feed.ts
@@ -12,6 +12,10 @@ router.get("/posts", feedController.getPosts);
 // POST /feed/post
 router.post("/post", validationAddPost, feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost)
+router.get("/post/:postId", feedController.getPost);
+
+router.put("/post/:postId", validationAddPost, feedController.updatePost);
+
+router.delete("/post/:postId", feedController.deletePost);
 
 module.exports = router;
