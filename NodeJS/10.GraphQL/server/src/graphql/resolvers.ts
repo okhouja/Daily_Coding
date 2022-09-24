be20f29d547ff5f57422eba5ab8c0f2841eb4962
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-09-24 13:05:28 +0200

    Adding Pagination

--- NodeJS/10.GraphQL/server/src/graphql/resolvers.ts
+++ NodeJS/10.GraphQL/server/src/graphql/resolvers.ts
@@ -142,14 +142,23 @@ export default {
       updatedAt: createdPost.updatedAt?.toISOString(),
     };
   },
-  posts: async function (args: any, req: Request) {
+  posts: async function ({ page }: { page: number }, req: Request) {
     if (!req.isAuth) {
       const error = new Error("Not authenticated!");
       error.code = 401;
       throw error;
     }
+    if (!page) {
+      page = 1;
+    }
+    const perPage = 2;
+
     const totalPosts = await Post.find().countDocuments();
-    const posts = await Post.find().sort({ createdAt: -1 }).populate("creator");
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate("creator");
     return {
       posts: posts.map((p) => {
         return {
--- NodeJS/10.GraphQL/server/src/graphql/schema.ts
+++ NodeJS/10.GraphQL/server/src/graphql/schema.ts
@@ -44,7 +44,7 @@ export default buildSchema(`
     }
     type RootQuery {
         login(email:String!, password: String!) : AuthData!
-        posts: PostData!
+        posts(page:Int!): PostData!
        }
 
     type RootMutation {
