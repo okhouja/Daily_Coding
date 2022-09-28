4562a1c6bb7b1dd913f553f0f6c8a5d4e8e0ac47
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-09-28 15:20:42 +0200

    start a server using Deno

--- NodeJS/13.Deno/app.ts
+++ NodeJS/13.Deno/app.ts
@@ -1,4 +1,18 @@
-let message: string;
-message = "Hi there!";
+// // let message: string;
+// // message = "Hi there!";
 
-console.log(message);
+// // console.log(message);
+
+// const text =
+//   "This is a test message from Deno! and it should be stored in a file";
+
+// Deno.writeFile('message.txt', text);
+
+
+import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+ 
+function handler(_req: Request): Response {
+  return new Response("Hello World!! \n :)");
+}
+ 
+serve(handler, { port: 3000 });
