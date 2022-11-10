d39a002b4eb107c16ba195beaafa58df4c586e7a
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-11-10 20:46:45 +0100

    tests with multiple Expectations

--- JavaScript_Unit_Testing/1.Basic-testing/basic-testing-frontend/src/util/numbers.test.js
+++ JavaScript_Unit_Testing/1.Basic-testing/basic-testing-frontend/src/util/numbers.test.js
@@ -3,8 +3,8 @@ import { it, expect } from "vitest";
 import { transformToNumber } from './numbers'
 
 it('should transform a string numbers to number of type number', () => {
-    // const input = '1';   will pass
-    const input = 'invalid';  // will pass too
+    const input = '1';  // will pass
+    // const input = 'invalid';  // will pass too
 
 
     const result = transformToNumber(input)
@@ -12,16 +12,28 @@ it('should transform a string numbers to number of type number', () => {
     expect(result).toBeTypeOf('number')
 })
 
+it('should transform a string numbers to number of type number', () => {
+    const input = '1';  // will pass
+
+    const result = transformToNumber(input)
+
+    expect(result).toBe(+input)
+})
+
 it('should yield Nan for non-transformable values', () => {
     const input = 'invalid'
+    const input2 = {}
 
     const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
 
     expect(result).toBeNaN();
+    expect(result2).toBeNaN();
 })
 
 it('should yield NaN if no value is passed into the function', () => {
     const result = transformToNumber();
-   
+
     expect(result).toBeNaN();
-  });
+});
