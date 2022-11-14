63b83e3ce3cff4096ee829e9e2de29991b22f027
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-11-14 18:47:31 +0100

    Refacorting & Added output Test

--- JavaScript_Unit_Testing/1.Basic-testing/basic-testing-frontend/src/util/numbers.js
+++ JavaScript_Unit_Testing/1.Basic-testing/basic-testing-frontend/src/util/numbers.js
@@ -1,5 +1,17 @@
+import {validateStringNotEmpty,validateNumber} from './validation'
 export function transformToNumber(value) {
   // return NaN
   return +value;
 }
 
+export function cleanNumber(numberValues) {
+  const numbers = [];
+  for (const numberInput of numberValues) {
+    validateStringNotEmpty(numberInput);
+    const number = transformToNumber(numberInput);
+    validateNumber(number);
+    numbers.push(number);
+
+  }
+  return numbers;
+}
