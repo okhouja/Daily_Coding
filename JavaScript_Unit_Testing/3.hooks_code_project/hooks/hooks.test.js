3099bae356177b2f0f092f08f8cca061f2b0b3d0
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-11-17 15:04:54 +0100

    Working with Mocking & Spies

--- JavaScript_Unit_Testing/3.hooks_code_project/hooks/hooks.test.js
+++ JavaScript_Unit_Testing/3.hooks_code_project/hooks/hooks.test.js
@@ -22,7 +22,18 @@ afterEach(() => {
   console.log('afterEach');
 })
 
-it("should update the email", () => {
+
+// describe.concurrent() // use concurrent to speed up tests & execute in parallel / and if you use describe you should not need it in it function 
+/* 
+
+Concurrency & Default Behavior
+Even when not adding the .concurrent property / annotation, tests that are stored in different files are executed concurrently (i.e., in parallel). This is done by both Vitest and Jest - ensuring that your tests run in a short amount of time.
+
+With .concurrent you can enforce this behavior also inside the individual files (i.e., tests that live in one and the same file are executed concurrently).
+
+Concurrent execution can reduce the amount of time your tests need to execute. A downside of concurrent execution is, that tests that perform clashing (global) state manipulations may interfere with each other.
+*/
+it.concurrent("should update the email", () => {
   const newTestEmail = "test2@test.com";
 
   user.updateEmail(newTestEmail);
@@ -30,21 +41,21 @@ it("should update the email", () => {
   expect(user.email).toBe(newTestEmail);
 });
 
-it("should have an email property", () => {
+it.concurrent("should have an email property", () => {
   expect(user).toHaveProperty("email");
 });
 
-it("should store the provided email value", () => {
+it.concurrent("should store the provided email value", () => {
   expect(user.email).toBe(testEmail);
 });
 
-it("should clear the email", () => {
+it.concurrent("should clear the email", () => {
   user.clearEmail();
 
   expect(user.email).toBe("");
 });
 
-it("should still have an email property after clearing the email", () => {
+it.concurrent("should still have an email property after clearing the email", () => {
   user.clearEmail();
 
   expect(user).toHaveProperty("email");
