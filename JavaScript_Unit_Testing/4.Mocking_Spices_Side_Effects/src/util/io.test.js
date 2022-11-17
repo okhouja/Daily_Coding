462fe043b3a29edc142ef0ceb2de69bc7521bd15
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-11-17 15:51:56 +0100

    added __mocking__ folder to custom Mock implementation Globally

--- JavaScript_Unit_Testing/4.Mocking_Spices_Side_Effects/src/util/io.test.js
+++ JavaScript_Unit_Testing/4.Mocking_Spices_Side_Effects/src/util/io.test.js
@@ -20,7 +20,18 @@ it("should execute the writeFile method", () => {
 
     writeData(testData, testFilename)
 
-    //   expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+    //  expect(writeData(testData, testFilename)).resolves.toBeUndefined();
     // expect(fs.writeFile).toBeCalled()
     expect(fs.writeFile).toBeCalledWith(testFilename, testData)
+});
+
+it("should return a promise that resolves to no value if called correctly", () => {
+    const testData = "Test";
+    const testFilename = "test.txt";
+
+    writeData(testData, testFilename)
+
+    expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+    // expect(fs.writeFile).toBeCalled()
+    // expect(fs.writeFile).toBeCalledWith(testFilename, testData)
 });
