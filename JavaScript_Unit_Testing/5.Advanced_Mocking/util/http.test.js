101b2fa678d3ffddd6b2b531cb2ef75a742da3c5
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-11-18 14:46:05 +0100

    test-specific Mocking Logic

--- JavaScript_Unit_Testing/5.Advanced_Mocking/util/http.test.js
+++ JavaScript_Unit_Testing/5.Advanced_Mocking/util/http.test.js
@@ -4,22 +4,39 @@ import { sendDataRequest } from "./http";
 const testRespondeData = { testKey: "testdata" };
 
 const testFetch = vi.fn((url, options) => {
-  return new Promise((resolve, reject) => {
-    const testResponse = {
-      ok: true,
-      json() {
-        return new Promise((resolve, reject) => {
-          resolve(testRespondeData);
-        });
-      },
-    };
-    resolve(testResponse);
-  });
-});
+    if (typeof options.body !== 'string') {
+        return reject('Not a string');
+    }
+    return new Promise((resolve, reject) => {
+        const testResponse = {
+            ok: true,
+            json() {
+                return new Promise((resolve, reject) => {
+                    resolve(testRespondeData);
+                });
+            },
+        };
+        resolve(testResponse);
+    });
+}
+);
 
 vi.stubGlobal("fetch", testFetch);
-it("should return any available response data", () => {
-  const testData = { key: "test" };
 
-  return expect(sendDataRequest(testData)).resolves.toEqual(testRespondeData);
+it("should return any available response data", () => {
+    const testData = { key: "test" };
+
+    return expect(sendDataRequest(testData)).resolves.toEqual(testRespondeData);
+});
+
+it("should convert the provided data to JSON before sending the request", async () => {
+    const testData = { key: "test" };
+    let errorMessage;
+
+    try {
+        await sendDataRequest(testData)
+    } catch (error) {
+        errorMessage = error;
+    }
+    return expect(errorMessage).not.toBe('Not a string.');
 });
