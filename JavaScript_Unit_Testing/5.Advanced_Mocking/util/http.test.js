fadf2aea5c3fd95fe2b334af97c5124869c39f7d
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2022-11-18 14:27:08 +0100

    Mocking Global Values & Functions

---
+++ JavaScript_Unit_Testing/5.Advanced_Mocking/util/http.test.js
@@ -0,0 +1,25 @@
+import { describe, it, expect, vi } from "vitest";
+import { sendDataRequest } from "./http";
+
+const testRespondeData = { testKey: "testdata" };
+
+const testFetch = vi.fn((url, options) => {
+  return new Promise((resolve, reject) => {
+    const testResponse = {
+      ok: true,
+      json() {
+        return new Promise((resolve, reject) => {
+          resolve(testRespondeData);
+        });
+      },
+    };
+    resolve(testResponse);
+  });
+});
+
+vi.stubGlobal("fetch", testFetch);
+it("should return any available response data", () => {
+  const testData = { key: "test" };
+
+  return expect(sendDataRequest(testData)).resolves.toEqual(testRespondeData);
+});
