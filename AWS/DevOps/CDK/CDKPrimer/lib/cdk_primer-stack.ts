00413597aa8fb0720dd7b8a3b1e4d6f7e6883a75
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-16 13:29:02 +0200

    fixing CDK v2 syntax

--- AWS/DevOps/CDK/CDKPrimer/lib/cdk_primer-stack.ts
+++ AWS/DevOps/CDK/CDKPrimer/lib/cdk_primer-stack.ts
@@ -1,18 +1,38 @@
-import * as cdk from "@aws-cdk/core";
-import * as ec2 from "@aws-cdk/aws-ec2";
-import * as ecs from "@aws-cdk/aws-ecs";
-import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+// import { Construct } from 'constructs';
+// // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-// import * as cdk from 'aws-cdk-lib';
+// export class CdkPrimerStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     // The code that defines your stack goes here
+
+//     // example resource
+//     // const queue = new sqs.Queue(this, 'CdkPrimerQueue', {
+//     //   visibilityTimeout: cdk.Duration.seconds(300)
+//     // });
+//   }
+// }
+
+// import * as cdk from "@aws-cdk/core";
+// import * as ec2 from "@aws-cdk/aws-ec2";
+// import * as ecs from "@aws-cdk/aws-ecs";
+// import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+
+
+import * as cdk from 'aws-cdk-lib';
+
+import {aws_ecs as ecs,aws_ec2 as ec2,aws_ecs_patterns as ecs_patterns}  from 'aws-cdk-lib';
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkPrimerStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
+
     const cluster = new ecs.Cluster(this, "MyCluster", { vpc: vpc });
+
     new ecs_patterns.ApplicationLoadBalancedFargateService(
       this,
       "MyFargateService",
@@ -28,3 +48,4 @@ export class CdkPrimerStack extends cdk.Stack {
     );
   }
 }
+
