5bcfdae257b6b80ba42698d354c786086560d417
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-16 11:29:07 +0200

    setup cdk primer stack

--- AWS/DevOps/CDK/CDKPrimer/lib/cdk_primer-stack.ts
+++ AWS/DevOps/CDK/CDKPrimer/lib/cdk_primer-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+
+// import * as cdk from 'aws-cdk-lib';
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkPrimerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, "MyCluster", { vpc: vpc });
+    new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      "MyFargateService",
+      {
+        cluster: cluster,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPrimerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+        },
+
+        publicLoadBalancer: true,
+      }
+    );
   }
 }
