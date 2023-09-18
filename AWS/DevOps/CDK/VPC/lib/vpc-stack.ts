8c15b36df1d12aa1052cc37145d4b6882b44cfde
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-18 12:15:36 +0200

    install ec2.CfnVPC

--- AWS/DevOps/CDK/VPC/lib/vpc-stack.ts
+++ AWS/DevOps/CDK/VPC/lib/vpc-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+// import {aws_ecs as ecs,aws_ec2 as ec2,aws_ecs_patterns as ecs_patterns}  from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +10,7 @@ export class VpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ec2.CfnVPC(this, 'MyVPC', { cidrBlock: '10.0.0.0/16'})
+   
   }
 }
