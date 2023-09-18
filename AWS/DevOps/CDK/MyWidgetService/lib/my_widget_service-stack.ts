122b7ca9f4029c389c29cf7978208918c59c1563
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-18 15:48:11 +0200

    add lambda function to Project

--- AWS/DevOps/CDK/MyWidgetService/lib/my_widget_service-stack.ts
+++ AWS/DevOps/CDK/MyWidgetService/lib/my_widget_service-stack.ts
@@ -1,16 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
+// import * as cdk from 'aws-cdk-lib';
+import * as widget_service from '../lib/widget_service';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyWidgetServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyWidgetServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new widget_service.WidgetService(this, 'Widgets');
   }
 }
