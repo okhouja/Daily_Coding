25e1e40704ca99b64d1f0ffe09ec17e43832d831
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-18 13:30:52 +0200

    configure ec2.VPC with subnet & EIP &...

--- AWS/DevOps/CDK/VPC/lib/vpc-stack.ts
+++ AWS/DevOps/CDK/VPC/lib/vpc-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
 // import {aws_ecs as ecs,aws_ec2 as ec2,aws_ecs_patterns as ecs_patterns}  from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+// import { IpAddresses } from "aws-cdk-lib/aws-ec2";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
-
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class VpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new ec2.CfnVPC(this, 'MyVPC', { cidrBlock: '10.0.0.0/16'})
-   
+    // new ec2.CfnVPC(this, 'MyVPC', { cidrBlock: '10.0.0.0/16'})
+
+    // new ec2.Vpc(this, "MyVPC", { cidr: "10.0.0.0/16" });
+
+    new ec2.Vpc(this, "MyVPC", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+    });
   }
 }
