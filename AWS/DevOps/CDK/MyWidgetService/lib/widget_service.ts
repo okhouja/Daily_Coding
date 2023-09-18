3f4f921d28ec8fe58c807cf67ee35cc7242a9451
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-18 17:12:48 +0200

    adding widget.js & fixing runtime error -downgrade from 18 to 16 to work with sdk v2

--- AWS/DevOps/CDK/MyWidgetService/lib/widget_service.ts
+++ AWS/DevOps/CDK/MyWidgetService/lib/widget_service.ts
@@ -1,5 +1,5 @@
 // import * as core from "@aws-cdk/core";
-import * as core from 'aws-cdk-lib';
+import * as core from "aws-cdk-lib";
 
 // import * as apigateway from "@aws-cdk/aws-apigateway";
 // import * as lambda from "@aws-cdk/aws-lambda";
@@ -9,8 +9,7 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
-import { Construct } from 'constructs';
-
+import { Construct } from "constructs";
 
 export class WidgetService extends Construct {
   constructor(scope: Construct, id: string) {
@@ -19,25 +18,46 @@ export class WidgetService extends Construct {
     const bucket = new s3.Bucket(this, "WidgetStore");
 
     const handler = new lambda.Function(this, "WidgetHandler", {
-      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
+      runtime: lambda.Runtime.NODEJS_16_X, // So we can use async in widget.js
       code: lambda.Code.fromAsset("resources"),
       handler: "widgets.main",
       environment: {
-        BUCKET: bucket.bucketName
-      }
+        BUCKET: bucket.bucketName,
+      },
     });
 
     bucket.grantReadWrite(handler); // was: handler.role);
 
     const api = new apigateway.RestApi(this, "widgets-api", {
       restApiName: "Widget Service",
-      description: "This service serves widgets."
+      description: "This service serves widgets.",
     });
 
     const getWidgetsIntegration = new apigateway.LambdaIntegration(handler, {
-      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' },
     });
 
     api.root.addMethod("GET", getWidgetsIntegration); // GET /
+
+    // Connecting to API Gateway
+    const widget = api.root.addResource("{id}");
+
+    // Add new widget to bucket with: POST /{id}
+    const postWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+    // Get a specific widget from bucket with: GET /{id}
+    const getWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+    // Remove a specific widget from the bucket with: DELETE /{id}
+    const deleteWidgetIntegration = new apigateway.LambdaIntegration(handler);
+
+    // POST /{id}
+    widget.addMethod("POST", postWidgetIntegration);
+
+    // GET /{id}
+    widget.addMethod("GET", getWidgetIntegration);
+
+    // DELETE /{id}
+    widget.addMethod("DELETE", deleteWidgetIntegration);
   }
 }
