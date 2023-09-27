161d40d3b83d45bb5413f93570fba635cbb83ea7
Omar Khouja <75792364+okhouja@users.noreply.github.com>
2023-09-27 19:59:18 +0200

    finishing CDK testing

--- AWS/DevOps/CDK/TestingConstructs/lib/dead-letter-queue.ts
+++ AWS/DevOps/CDK/TestingConstructs/lib/dead-letter-queue.ts
@@ -5,30 +5,48 @@ import { Stack, StackProps,Duration } from 'aws-cdk-lib';
 import * as sqs from 'aws-cdk-lib/aws-sqs'
 import { Construct } from 'constructs';
 
-export class DeadLetterQueue extends Stack {
-  public readonly messagesInQueueAlarm: cloudwatch.IAlarm;
+// export class DeadLetterQueue extends Stack {
+//   public readonly messagesInQueueAlarm: cloudwatch.IAlarm;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+//   constructor(scope: Construct, id: string, props?: StackProps) {
+//     super(scope, id, props);
 
 
-    // const metric = queue.metric("ApproximateNumberOfMessagesVisible");
-    const metric = new cloudwatch.Metric({
-        namespace: 'MyNamespace',
-        metricName: 'MyMetric',
-        dimensionsMap: { MyDimension: 'MyDimensionValue' },
+//     // const metric = queue.metric("ApproximateNumberOfMessagesVisible");
+//     const metric = new cloudwatch.Metric({
+//         namespace: 'MyNamespace',
+//         metricName: 'MyMetric',
+//         dimensionsMap: { MyDimension: 'MyDimensionValue' },
 
-        // add a period property of 6 minute to override the default of 5 minutes
-        period: Duration.minutes(6)
-      });
+//         // add a period property of 6 minute to override the default of 5 minutes
+//         period: Duration.minutes(6)
+//       });
 
-    // Add the alarm
-    this.messagesInQueueAlarm = new cloudwatch.Alarm(this, 'Alarm', {
-      alarmDescription: 'There are messages in the Dead Letter Queue',
-      evaluationPeriods: 1,
-      threshold: 1,
-      metric: metric,
+//     // Add the alarm
+//     this.messagesInQueueAlarm = new cloudwatch.Alarm(this, 'Alarm', {
+//       alarmDescription: 'There are messages in the Dead Letter Queue',
+//       evaluationPeriods: 1,
+//       threshold: 1,
+//       metric: metric,
       
-    });
+//     });
+//   }
+// }
+
+export interface DeadLetterQueueProps{
+  retentionDays?: number;
+}
+export class DeadLetterQueue extends sqs.Queue {
+public readonly messagesInQueueAlarm: cloudwatch.IAlarm;
+
+constructor(scope: Construct, id: string, props: DeadLetterQueueProps = {}) {
+  if (props.retentionDays !== undefined && props.retentionDays > 14) {
+    throw new Error('retentionDays may not exceed 14 days');
   }
+
+  super(scope, id, {
+      // Given retention period or maximum
+      retentionPeriod: Duration.days(props.retentionDays || 14)
+  });
+}
 }
--- AWS/DevOps/CDK/TestingConstructs/test/dead-letter-queue.test.ts
+++ AWS/DevOps/CDK/TestingConstructs/test/dead-letter-queue.test.ts
@@ -1,9 +1,10 @@
 // import * as cdk from 'aws-cdk-lib';
 // import '@aws-cdk/assert/jest';
-import { Template } from 'aws-cdk-lib/assertions';
-import {App } from 'aws-cdk-lib/core';
+// import { Capture,Match,Template } from 'aws-cdk-lib/assertions';
+// import {App,Stack} from 'aws-cdk-lib/core';
 
-import {DeadLetterQueue} from '../lib/dead-letter-queue';
+// import {DeadLetterQueue } from '../lib/dead-letter-queue';
+// import dlq = require('../lib/dead-letter-queue');
 
 // test('dlq creates an alarm', () => {
 //   const app = new App();
@@ -17,3 +18,73 @@ import {DeadLetterQueue} from '../lib/dead-letter-queue';
 //     Namespace: 'AWS/SQS',
 // }))
 // });
+
+
+// test('retention period can be configured', () => {
+//   const app = new cdk.App();
+//   const dlqStack = new DeadLetterQueue(app, 'DLQ', {
+//     retentionDays: 7
+//   })
+//   const template = Template.fromStack(dlqStack)
+
+//   // new DeadLetterQueue(app, 'DLQ', {
+//   //   retentionDays: 7
+//   // });
+
+//   expect(app.hasResource('AWS::SQS::Queue', {
+//     MessageRetentionPeriod: 604800
+//   }))
+
+//   // expect(dlqStack).toHaveResource('AWS::SQS::Queue', {
+//   //   MessageRetentionPeriod: 604800
+//   // });
+// });
+
+// test('configurable retention period cannot exceed 14 days', () => {
+//   const stack = new App();
+
+//   expect(() => {
+//      new DeadLetterQueue(stack, 'DLQ', {
+//       retentionDays: 15
+//     });
+//   }).toThrowError(/retentionDays may not exceed 14 days/);
+// });
+
+
+
+// new code
+import * as cdk from 'aws-cdk-lib';
+
+// import '@aws-cdk/assert/jest';
+// import {App,Stack} from 'aws-cdk-lib/core';
+
+// import { Stack } from '@aws-cdk/core';
+import { Capture,Match,Template } from 'aws-cdk-lib/assertions';
+
+
+import dlq = require('../lib/dead-letter-queue');
+
+let app: cdk.App, stack: cdk.Stack, template: Template;
+
+
+test('retention period can be configured', () => {
+  // const app = new App();
+
+  new dlq.DeadLetterQueue(app, 'DLQ', {
+    retentionDays: 7
+  });
+
+  expect(template.hasResource('AWS::SQS::Queue', {
+    MessageRetentionPeriod: 604800
+  }))
+});
+
+test('configurable retention period cannot exceed 14 days', () => {
+  // const app = new App();
+
+  expect(() => {
+    new dlq.DeadLetterQueue(stack, 'DLQ', {
+      retentionDays: 15
+    });
+  }).toThrowError(/retentionDays may not exceed 14 days/);
+});
