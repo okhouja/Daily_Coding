bcc95f7ab65cf370795a16a7719bfa9012a39434
omar0940 <omar0940@gmail.com>
2025-03-07 10:46:20 +0100

    Add new SQL files

---
+++ SQL/03.insert-users-data.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO users (yearly_salary, full_name, current_status)
+-- VALUES (19000, 'Omar Kh', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (25000, 'Max Pain', 'employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (0, 'Michael Smith', 'unemployed');
