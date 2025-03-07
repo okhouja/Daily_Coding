0aad8f41305f4717d0062ec3accc734b0911403c
omar0940 <omar0940@gmail.com>
2025-03-07 20:46:53 +0100

    Learing Go Lang

---
+++ SQL/11.update-tables.sql
@@ -0,0 +1,11 @@
+-- ALTER TABLE employers
+-- ALTER COLUMN yearly_revenue SET DATA TYPE FLOAT; -- Postgresql
+
+-- ALTER TABLE employers
+-- MOdify COLUMN yearly_revenue FLOAT; -- MYSQL
+
+ALTER TABLE users
+ALTER COLUMN full_name SET DATA TYPE VARCHAR(300); -- Postgresql
+
+-- ALTER TABLE users
+-- MOdify COLUMN full_name VARCHAR(300);  -- MYSQL
---
+++ SQL/14.inserting-with-constraints.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (full_name,current_status)
+VALUES ('Manual Lorenz','self-employed')
