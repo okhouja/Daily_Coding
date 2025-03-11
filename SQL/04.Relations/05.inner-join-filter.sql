bc6c4008d5115e3c77a7688998fb9b7a5d81294d
omar0940 <omar0940@gmail.com>
2025-03-11 17:36:18 +0100

    Data Joining & Filtering

---
+++ SQL/04.Relations/05.inner-join-filter.sql
@@ -0,0 +1,7 @@
+-- SELECT u.id, first_name, last_name, street, house_number, city_id
+SELECT u.id, first_name, last_name, street, house_number, c.name AS city_name
+FROM users AS u
+INNER JOIN adresses AS a ON u.adress_id = a.id
+INNER JOIN cities AS c ON a.city_id = c.id
+WHERE c.id = 2 OR c.id = 3
+ORDER BY u.id DESC;
