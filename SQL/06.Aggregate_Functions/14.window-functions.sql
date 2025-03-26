c621e972aafede126a491433b75f8bfe5c33ff51
omar0940 <omar0940@gmail.com>
2025-03-26 12:25:20 +0100

    Working with ORDER BY & RANK

--- SQL/06.Aggregate_Functions/14.window-functions.sql
+++ SQL/06.Aggregate_Functions/14.window-functions.sql
@@ -3,8 +3,16 @@
 
 -- SELECT SUM(amount_tipped) FROM bookings;
 
--- SELECT booking_date, amount_billed, SUM(amount_tipped) OVER()
+-- SELECT booking_date, amount_tipped SUM(amount_tipped) OVER()
 -- FROM bookings;
 
-SELECT booking_date, amount_billed, SUM(amount_tipped) OVER(PARTITION BY booking_date)
+-- SELECT booking_date, amount_billed, SUM(amount_tipped) OVER(PARTITION BY booking_date)
+-- FROM bookings;
+
+-- SELECT booking_date, amount_tipped, SUM(amount_tipped)
+-- OVER(PARTITION BY booking_date ORDER BY amount_billed)
+-- FROM bookings;
+
+SELECT booking_date, amount_tipped, RANK()
+OVER(PARTITION BY booking_date ORDER BY amount_tipped DESC)
 FROM bookings;
