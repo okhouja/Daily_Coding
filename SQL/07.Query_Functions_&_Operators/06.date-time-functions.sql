c22c4eadac6246ba102cbb25df0bea57132c5346
omar0940 <omar0940@gmail.com>
2025-03-30 18:22:34 +0200

    Working with Extract DATE and TIME and WORKDAY

--- SQL/07.Query_Functions_&_Operators/06.date-time-functions.sql
+++ SQL/07.Query_Functions_&_Operators/06.date-time-functions.sql
@@ -1,2 +1,21 @@
-SELECT EXTRACT(MONTH FROM last_checkin)
+-- SELECT EXTRACT(MONTH FROM last_checkin)
+-- FROM memberships;
+
+-- DOW for Weekday (start with Sunday as 0 ) and ISODOW for Weekday (start with Monday as 1)
+-- this is work only on PosgreSQL and Monday will show 1 in both example if there no Sunday
+-- SELECT EXTRACT(ISODOW FROM last_checkin)
+-- FROM memberships;
+
+-- this work only in MySQL and Monday will start as 0 .. so we need to add +1 to begin with 1
+-- SELECT WEEKDAY(last_checkin) +1, last_checkin
+-- FROM memberships;
+
+-- this work only on MYSQL and this is to EXTRACT DATE and TIME
+-- SELECT CONVERT(last_checkin, DATE), CONVERT(last_checkin, TIME)
+-- FROM memberships;
+
+-- this work only on PosgreSQL and this is to EXTRACT DATE and TIME
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
 FROM memberships;
+
+
