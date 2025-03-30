dfe2dc6e508ed061fd4e366e23397cf853f84af2
omar0940 <omar0940@gmail.com>
2025-03-30 15:42:57 +0200

    Working with String Function With SELECT

---
+++ SQL/07.Query_Functions_&_Operators/05.string-functions.sql
@@ -0,0 +1,10 @@
+-- SELECT CONCAT(first_name,' ',last_name)
+-- FROM memberships;
+
+
+-- PostgreSQL only
+-- SELECT first_name || ' ' || last_name
+-- FROM memberships;
+
+SELECT CONCAT('$ ', price)
+FROM memberships;
