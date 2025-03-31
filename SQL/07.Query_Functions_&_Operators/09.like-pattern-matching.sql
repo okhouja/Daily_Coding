a08f8496e8dca8ccf0728f39cc879e83a22df572
omar0940 <omar0940@gmail.com>
2025-03-31 19:06:59 +0200

    Working with LIKE & Pattern Matching

---
+++ SQL/07.Query_Functions_&_Operators/09.like-pattern-matching.sql
@@ -0,0 +1,13 @@
+-- SELECT first_name LIKE '_a%', first_name
+-- FROM memberships;
+
+SELECT first_name LIKE 'ma%', first_name
+FROM memberships;
+
+-- postgreSQL
+-- SELECT first_name ILIKE 'ma%', first_name
+-- FROM memberships;
+
+-- SELECT first_name
+-- FROM memberships
+-- WHERE first_name LIKE 'J___';
