202f3b932ed43b19ff50533de9b9bddbeb1ad0e2
omar0940 <omar0940@gmail.com>
2025-04-02 10:17:35 +0200

    Working with Subquery and EXISTS #2

--- SQL/07.Query_Functions_&_Operators/10.subquery-expressions.sql
+++ SQL/07.Query_Functions_&_Operators/10.subquery-expressions.sql
@@ -61,8 +61,16 @@
 -- FROM customers
 -- WHERE email = 'max@test.com';
 
-SELECT EXISTS(
-    SELECT first_name, last_name
-    FROM customers
-    WHERE email = 'max@test.com'
+-- SELECT EXISTS(
+--     SELECT first_name, last_name
+--     FROM customers
+--     WHERE email = 'max@test.com'
+-- );
+
+SELECT o.id
+FROM orders AS o
+WHERE EXISTS(
+    SELECT c.email
+    FROM customers AS c
+    WHERE o.customer_id = c.id AND c.email = 'max@test.com'
 );
