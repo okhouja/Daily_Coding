9032db301abc9f55c9e499f2405dd0b6fcef529c
omar0940 <omar0940@gmail.com>
2025-04-03 19:57:48 +0200

    Insert Data into event table

---
+++ SQL/11.Full_Demo_Project/03.manipulate-data.sql
@@ -0,0 +1,21 @@
+INSERT INTO events(
+    name,
+    date_planned,
+    description,
+    max_participants,
+    min_age
+  )
+VALUES (
+    'A First Event',
+    '2025-03-01 16:30:00',
+    'This is the description of this first event',
+    20,
+    18
+  ),
+  (
+    'A Second Event',
+    '2025-04-08 17:30:00',
+    'This is the description of this second event',
+    20,
+    18
+  );
