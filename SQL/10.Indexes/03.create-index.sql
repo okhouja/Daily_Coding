42d960350a9a1e48fc06d670457b6385f9f1ba5b
omar0940 <omar0940@gmail.com>
2025-04-03 11:35:42 +0200

    UNIQUE INDEXES

--- SQL/10.Indexes/03.create-index.sql
+++ SQL/10.Indexes/03.create-index.sql
@@ -1,3 +1,7 @@
-CREATE INDEX salaryidx ON users(salary);
+-- CREATE INDEX salaryidx ON users(salary);
+
+-- Use UNIQUE when you want to create a unique column like email address...
+-- and when you create UNIQUE, you already create an index for it."
+CREATE UNIQUE INDEX salaryidx ON users(salary); 
 
 -- DROP INDEX salaryidx;
