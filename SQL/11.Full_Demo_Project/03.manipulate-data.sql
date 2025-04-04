9dae6ae26a959a657bb22da7fdde96f093ee8ad0
omar0940 <omar0940@gmail.com>
2025-04-04 08:30:07 +0200

    Insert & Update data into events TABLE

--- SQL/11.Full_Demo_Project/03.manipulate-data.sql
+++ SQL/11.Full_Demo_Project/03.manipulate-data.sql
@@ -1,21 +1,28 @@
-INSERT INTO events(
-    name,
-    date_planned,
-    description,
-    max_participants,
-    min_age
-  )
-VALUES (
-    'A First Event',
-    '2025-03-01 16:30:00',
-    'This is the description of this first event',
-    20,
-    18
-  ),
-  (
-    'A Second Event',
-    '2025-04-08 17:30:00',
-    'This is the description of this second event',
-    20,
-    18
-  );
+-- INSERT INTO events(
+--     name,
+--     date_planned,
+--     description,
+--     max_participants,
+--     min_age
+--   )
+-- VALUES (
+--     'A First Event',
+--     '2025-03-01 16:30:00',
+--     'This is the description of this first event',
+--     20,
+--     18
+--   ),
+--   (
+--     'A Second Event',
+--     '2025-04-08 17:30:00',
+--     'This is the description of this second event',
+--     20,
+--     18
+--   );
+
+UPDATE events
+SET min_age = 16, description = 'This is the description of this second event'
+WHERE id = 2;
+
+-- DELETE FROM events
+-- WHERE id = 1;
--- SQL/11.Full_Demo_Project/04.quere-data.sql
+++ SQL/11.Full_Demo_Project/04.quere-data.sql
@@ -1,1 +1,2 @@
-SELECT * FROM events;
+SELECT * FROM events
+WHERE date_planned > '2025-04-01' OR min_age = 18;
