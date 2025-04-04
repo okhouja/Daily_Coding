54e8dfecb2f55c4b2ee60528ca058449a6e22115
omar0940 <omar0940@gmail.com>
2025-04-04 17:08:09 +0200

    Finishing MYSQL & PostgreSQL Course

--- SQL/11.Full_Demo_Project/11.more-queries.sql
+++ SQL/11.Full_Demo_Project/11.more-queries.sql
@@ -1,3 +1,15 @@
-SELECT * FROM users
--- WHERE first_name LIKE 'Ma%';
-WHERE first_name LIKE 'Ma_';
+-- SELECT * FROM users
+-- -- WHERE first_name LIKE 'Ma%';
+-- WHERE first_name LIKE 'Ma_';
+
+-- SELECT COUNT(id) FROM locations;
+
+-- SELECT c.name,loc.street ,COUNT(loc.id) AS num_locations FROM cities AS c
+-- LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- GROUP BY c.name, loc.street;
+
+SELECT c.name, ,COUNT(loc.id) AS num_locations FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+WHERE loc.city_name = 'Munich' 
+GROUP BY c.name;
+HAVING COUNT(loc.id) > 1;
